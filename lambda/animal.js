8330894e597f985133a694ee6eac8434073e573a
Mik Linson <miklinson@gmail.com>
2021-07-17 16:27:18 +0800

    feat: Lambda and API GW integration

--- lambda/animal.js
+++ lambda/animal.js
@@ -1,5 +1,17 @@
-exports.handler =  async function(event, context) {
-    console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-    return context.logStreamName
-  }
-  
+exports.handler = async function (event, context) {
+    console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+    let animals = ["Lion", "Tiger", "Orca", "Black Mamba", "Puma", "Lobster", "Eagle", "Falcon", "Pigeon"];
+    const random = Math.floor(Math.random() * animals.length);
+    let responseBody = {
+        success: true,
+        animal: animals[random]
+    };
+    let response = {
+        statusCode: 200,
+        headers: {
+            "x-custom-header": "custom-header-value"
+        },
+        body: JSON.stringify(responseBody)
+    };
+    return response;
+}
