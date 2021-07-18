70b9e0a39f8c0e4a2da9dc08d24ba9146d81c06d
Mik Linson <miklinson@gmail.com>
2021-07-18 15:21:00 +0800

    refactor: Optimize the responseBody

--- lambda/refresh/refresh.js
+++ lambda/refresh/refresh.js
@@ -1,12 +1,9 @@
 const jwt = require('jsonwebtoken');
 exports.handler = async function (event, context) {
-    responseBody = JSON.stringify(event);
+    responseBody = event.body;
     console.log(responseBody);
     let response = {
         statusCode: 200,
-        headers: {
-            "x-custom-header": "custom-header-value"
-        },
         body: responseBody
     };
     return response;
