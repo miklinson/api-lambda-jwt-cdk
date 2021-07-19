7c39556089316aaed4f2096ba929e89f546d291b
Mik Linson <miklinson@gmail.com>
2021-07-19 21:53:04 +0800

    feat: Add main logic in Logout Lambda

--- lambda/logout/logout.js
+++ lambda/logout/logout.js
@@ -3,9 +3,60 @@ const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async function (event, context) {
-    let response = {
-        statusCode: 200,
-        body: JSON.stringify(event)
+    //Init variables
+    let ddbError = false;
+    let response, responseBody = {};
+    let refreshToken;
+    //Get refresh_token in body
+    try {
+        const body = JSON.parse(event.body);
+        refreshToken = body.refresh_token; //no refresh token found in body
+    } catch (err) {
+        parseError = true;
+        responseBody = {
+            "message": err.message
+        }
+        response = {
+            statusCode: 403,
+            body: JSON.stringify(responseBody)
+        };
+    }
+    if (parseError) return response;
+
+    //Init parameters
+    let params = {
+        TableName: 'token',
+        Key: {
+            refreshToken: refreshToken
+        }
+    };
+    //Delete Item
+    try {
+        await deleteItem(params);
+    } catch (err) {
+        ddbError = true;
+        response = {
+            statusCode: 500,
+            body: JSON.stringify(err.message)
+        };
+    }
+    if (ddbError) return response; //cath error during DynamoDB action
+    //If no error
+    responseBody = {
+        "message": "token deleted!"
+    }
+    response = {
+        statusCode: 204,
+        body: JSON.stringify(responseBody)
     };
     return response;
+}
+
+async function deleteItem(params) {
+    try{
+        docClient.delete(params).promise()
+    } catch (err) {
+        return err;
+    }
+    
 }
