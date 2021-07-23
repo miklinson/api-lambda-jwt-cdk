0857e6056ed1fd5436c295df213096ac286ef129
Mik Linson <miklinson@gmail.com>
2021-07-23 10:00:40 +0800

    refactor: Add createToken function

--- lambda/refresh/refresh.js
+++ lambda/refresh/refresh.js
@@ -4,8 +4,8 @@ const docClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async function (event, context) {
     //Init variables
-    let ddbError, verifyError, parseError, noToken = false;
-    let jwtDecoded, data, refreshToken, jwtError;
+    let verifyError, noToken = false;
+    let jwtDecodedUsername, data, jwtError;
     let responseBody = {};
     let body, message;
     //Get refresh_token in body
@@ -35,24 +35,31 @@ exports.handler = async function (event, context) {
             verifyError = true;
             jwtError = err.message;
         } else {
-            jwtDecoded = decoded.user;
+            jwtDecodedUsername = decoded.user;
         }
     });
     if (verifyError) return response(403, jwtError);
 
     //If JWT verification succeeded
-    let user = { user: jwtDecoded };
+    let user = { user: jwtDecodedUsername };
+    let token = await createToken(user);
+    
+
+    responseBody = {
+        access_token: token.access,
+        token_type: "Bearer",
+        expires_in: token.expireTime
+    }
+    return response(200, null, responseBody);
+}
+
+async function createToken(user){
     let accessSecret = process.env.ACCESS_TOKEN_SECRET;
     let expireTime = parseInt(process.env.EXPIRES_IN, 10); //convert string to int
     let expires = { expiresIn: expireTime };
-    let accessToken = jwt.sign(user, accessSecret, expires);
-
-    responseBody = {
-        access_token: accessToken,
-        token_type: "Bearer",
-        expires_in: expireTime
-    }
-    return response(200, null, responseBody);
+    let access = jwt.sign(user, accessSecret, expires);
+    
+    return { access, expireTime }
 }
 
 function response(statusCode, message = null, respBody = null){
